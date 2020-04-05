7b2f838aa3f6141b1f29af4fd68dae71364f89a4
char <carvilsi.mail.list@gmail.com>
2020-04-05 15:54:24 +0200

    build working

--- src/backend/src/routes/build.js
+++ src/backend/src/routes/build.js
@@ -1,71 +1,71 @@
-const Router     = require('express-promise-router')
-const db         = require('../db')
-const { logger } = require('./../util/logger')
-const queries    = require('./../queries')
-const config     = require('config')
-const { check }  = require('./../util/requestChecker')
+const Router     = require('express-promise-router');
+const db         = require('../db');
+const { logger } = require('./../util/logger');
+const queries    = require('./../queries');
+const config     = require('config');
+const { check }  = require('./../util/requestChecker');
 
-const router   = new Router()
-module.exports = router
+const router   = new Router();
+module.exports = router;
 
 router.post('/', async (req, res) => {
   try {
     if (!check(req.body, ['desc', 'id_hospital'])) {
-      res.status(400).send('bad request for endpoint')
+      throw 'bad request for endpoint, mandatory: desc, id_hospital';
     }
-    const response = await db.query(queries.build.create,[req.body.desc, req.body.id_hospital])
-    req.body.id = response.rows[0].id
-    res.status(200).send(JSON.stringify(req.body))
+    const response = await db.query(queries.build.create,[req.body.desc, req.body.id_hospital]);
+    req.body.id = response.rows[0].id;
+    res.status(200).send(JSON.stringify(req.body));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.delete('/:id', async (req, res) => {
   try {
-    const { id } = req.params
-    await db.query(queries.build.delete,[id])
-    res.status(200).send({deleted: id})
+    const { id } = req.params;
+    await db.query(queries.build.delete,[id]);
+    res.status(200).send({deleted: id});
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.get('/', async (req, res) => {
   try {
-    const { id } = req.params
-    const offset = req.query.offset
-    const result = await db.query(queries.build.read,[offset, config.get('database.query.limit')])
-    res.status(200).send(JSON.stringify(result.rows))
+    const { id } = req.params;
+    const offset = req.query.offset;
+    const result = await db.query(queries.build.read,[offset, config.get('database.query.limit')]);
+    res.status(200).send(JSON.stringify(result.rows));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.get('/:id', async (req, res) => {
   try {
-    const { id } = req.params
-    const result = await db.query(queries.build.getById, [id])
-    res.status(200).send(JSON.stringify(result.rows))
+    const { id } = req.params;
+    const result = await db.query(queries.build.getById, [id]);
+    res.status(200).send(JSON.stringify(result.rows));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.put('/:id', async (req, res) => {
   try {
     if (!check(req.body, ['desc', 'id_hospital'])) {
-      res.status(400).send('bad request for endpoint')
+      throw 'bad request for endpoint, mandatory: desc, id_hospital';
     }
-    const { id } = req.params
-    const result = await db.query(queries.build.update, [req.body.desc, req.body.id_hospital, id])
-    res.status(200).send({updated: id})
+    const { id } = req.params;
+    const result = await db.query(queries.build.update, [req.body.desc, req.body.id_hospital, id]);
+    res.status(200).send({updated: id});
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
