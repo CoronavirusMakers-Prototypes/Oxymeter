942697b4f456eb4bebe7f949e5a3b2e6f3513b91
char <carvilsi.mail.list@gmail.com>
2020-04-12 13:35:55 +0200

    adding jwt to database on create user

--- src/backend/src/queries/index.js
+++ src/backend/src/queries/index.js
@@ -86,7 +86,7 @@ room = {
 }
 
 personal = {
-  create:  'INSERT INTO personal (surname, lastname, professional_id, last_login, id_role, login, password, id_hospital) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+  create:  'INSERT INTO personal (surname, lastname, professional_id, last_login, id_role, login, password, id_hospital, jwt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
   delete:  'DELETE FROM personal WHERE id = $1',
   update:  'UPDATE personal SET surname = $1, lastname = $2, professional_id = $3, last_login = $4, id_role = $5, login = $6, password = $7, id_hospital = $8 WHERE id = $9',
   read:    'SELECT * FROM personal WHERE 1=1 ORDER BY id',
--- src/backend/src/routes/personal.js
+++ src/backend/src/routes/personal.js
@@ -1,58 +1,67 @@
-const Router     = require('express-promise-router')
-const db         = require('../db')
-const { logger } = require('./../util/logger')
-const queries    = require('./../queries')
-const config     = require('config')
-const { check }  = require('./../util/requestChecker')
+const Router     = require('express-promise-router');
+const db         = require('../db');
+const { logger } = require('./../util/logger');
+const queries    = require('./../queries');
+const config     = require('config');
+const { check }  = require('./../util/requestChecker');
+const jwt        = require('jsonwebtoken');
 
-const router   = new Router()
-module.exports = router
+const router   = new Router();
+module.exports = router;
+
+jwtConfig = config.get('jwt')
+
 
 router.post('/', async (req, res) => {
   try {
     if (!check(req.body, ['surname', 'lastname', 'professional_id', 'last_login', 'id_role', 'login', 'password', 'id_hospital'])) {
       throw 'bad request for endpoint, mandatory: surname, lastname, professional_id, last_login, id_role, login, password, id_hospital';
     }
-    const response = await db.query(queries.personal.create,[req.body.surname, req.body.lastname, req.body.professional_id, req.body.last_login, req.body.id_role, req.body.login, req.body.password, req.body.id_hospital])
+    jwtGen = jwt.sign({login: req.body.login, password: req.body.password}, jwtConfig.secret, {algorithm: jwtConfig.algorithm, expiresIn: jwtConfig.expiresIn});
+    const response = await db.query(queries.personal.create,[req.body.surname, req.body.lastname, req.body.professional_id, req.body.last_login, req.body.id_role, req.body.login, req.body.password, req.body.id_hospital, jwtGen]);
     req.body.id = parseInt(response.rows[0].id);
-    res.status(200).send(JSON.stringify(req.body))
+    const userResponse = {
+      token: jwtGen,
+      user: req.body,
+    }
+    res.status(200).send(JSON.stringify(userResponse));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.delete('/:id', async (req, res) => {
   try {
-    const { id } = req.params
-    await db.query(queries.personal.delete,[id])
-    res.status(200).send({deleted: id})
+    const { id } = req.params;
+    await db.query(queries.personal.delete,[id]);
+    res.status(200).send({deleted: id});
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.get('/', async (req, res) => {
   try {
-    const { id } = req.params
-    const offset = req.query.offset
-    const result = await db.query(queries.personal.read)
-    res.status(200).send(JSON.stringify(result.rows))
+    const { id } = req.params;
+    const offset = req.query.offset;
+    const result = await db.query(queries.personal.read);
+    res.status(200).send(JSON.stringify(result.rows));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
 router.get('/:id', async (req, res) => {
   try {
-    const { id } = req.params
-    const result = await db.query(queries.personal.getById, [id])
-    res.status(200).send(JSON.stringify(result.rows))
+    const { id } = req.params;
+    const result = await db.query(queries.personal.getById, [id]);
+    res.status(200).send(JSON.stringify(result.rows));
   } catch (e) {
-    logger.error(e)
-    res.status(500).send(e)
+    logger.error(e);
+    res.status(500).send(e);
   }
 })
 
